950dba8e5c458f7ebe826a41db42aa100ada0591
guzzardi <mguzzardi@outlook.com>
2019-04-23 12:36:11 +0200

    Completed the module

---
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GradeBook.GradeBooks
+{
+    public class RankedGradeBook : BaseGradeBook
+    {
+        public RankedGradeBook(string name) : base(name)
+        { }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+                throw new InvalidOperationException();
+
+            var threshold = (int)Math.Ceiling(Students.Count * 0.2);
+
+            var grades = Students.OrderByDescending(e => e.AverageGrade).Select(e => e.AverageGrade).ToList();
+
+            if (averageGrade >= grades[threshold - 1])
+                return 'A';
+            else if (averageGrade >= grades[2*threshold - 1])
+                return 'B';
+            else if (averageGrade >= grades[3 * threshold - 1])
+                return 'C';
+            else if (averageGrade >= grades[4 * threshold - 1])
+                return 'D';
+            else return 'F';
+        }
+    }
+}
