6287795a18e0a67ce28016e4fffcd7947fcc93b4
guzzardi <mguzzardi@outlook.com>
2019-04-23 12:36:11 +0200

    Completed the module

---
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GradeBook.GradeBooks
+{
+    public class RankedGradeBook : BaseGradeBook
+    {
+        public RankedGradeBook(string name) : base(name)
+        { }
+    }
+}
